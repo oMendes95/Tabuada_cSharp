d189a546d1fd17f0753961f0510b27243412b37d
pdrlcx <mendesv460@gmail.com>
2022-07-18 10:29:43 -0300

    Adicionando

---
+++ Calculadora_Tabuada/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Calculadora_Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem Vindo a Calculadora\n Digite o numero que quer calcular: ");
+            int valor = int.Parse(Console.ReadLine());
+
+            //int y = 0;
+            for (int y = 0; y <= 10; y++)
+            {
+                int result = valor * y;
+                Console.WriteLine($"O Resultado de {valor} x {y}: {result}");
+            }
+
+
+
+
+
+
+        }
+    }
+}
